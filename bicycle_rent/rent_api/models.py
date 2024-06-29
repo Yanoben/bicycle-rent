e85e8ef9d299ca2072f8834ee9ed7ce945160bbf
Yanoben <yasinabdurakhmanov@gmail.com>
2024-06-29 09:20:21 +0300

    created models.py

--- bicycle_rent/rent_api/models.py
+++ bicycle_rent/rent_api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Bicycle(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.brand} {self.model}"
+
+class RentalHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bicycle = models.ForeignKey(Bicycle, on_delete=models.CASCADE)
+    rental_date = models.DateField()
+    return_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user} rented {self.bicycle}"
