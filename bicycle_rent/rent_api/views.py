cd3480d9788955feb6a4084e77b548134e2fec8c
Yanoben <yasinabdurakhmanov@gmail.com>
2024-06-29 09:20:39 +0300

    created views.py

--- bicycle_rent/rent_api/views.py
+++ bicycle_rent/rent_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Bicycle, RentalHistory
+from .serializers import UserSerializer, BicycleSerializer, RentalHistorySerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class BicycleViewSet(viewsets.ModelViewSet):
+    queryset = Bicycle.objects.all()
+    serializer_class = BicycleSerializer
+
+class RentalHistoryViewSet(viewsets.ModelViewSet):
+    queryset = RentalHistory.objects.all()
+    serializer_class = RentalHistorySerializer
